a91b21d62504a5cc1a191021ac1fa1edcf1f328f
J1991Ha <jasper.haag@icloud.com>
2024-01-24 11:41:31 +0100

    second cat

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -5,10 +5,7 @@ public class App {
     public static void main(String[] args) {
 
         Cat cat = new Cat();
-       /*
-         Cat cat2 = new Cat();
-        cat2.name = "Jasper";
-        */
+
         cat.name = "Alonzo";
         cat.furColor = "grey";
         cat.age = 35;
@@ -21,6 +18,20 @@ public class App {
 
         cat.SayHello();
         cat.tellYourAddress();
+        Cat cat2 = new Cat();
+
+        cat2.name = "Grizabella";
+        cat2.furColor = "white";
+        cat2.age = 29;
+
+
+
+        System.out.println(cat2.name);
+        System.out.println(cat2.age);
+        System.out.println(cat2.furColor);
+
+        cat2.SayHello();
+        cat2.tellYourAddress();
     /*
           cat2.tellYourAddress();
         cat2.tellyourName();

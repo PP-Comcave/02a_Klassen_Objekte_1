00cf5cda3107896166fab9107418ad667c56e10a
J1991Ha <jasper.haag@icloud.com>
2024-01-24 11:39:45 +0100

    cat with attr

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -5,13 +5,27 @@ public class App {
     public static void main(String[] args) {
 
         Cat cat = new Cat();
-        Cat cat2 = new Cat();
+       /*
+         Cat cat2 = new Cat();
+        cat2.name = "Jasper";
+        */
+        cat.name = "Alonzo";
+        cat.furColor = "grey";
+        cat.age = 35;
 
-        System.out.println(cat);
+
+
+        System.out.println(cat.name);
+        System.out.println(cat.age);
+        System.out.println(cat.furColor);
+
         cat.SayHello();
         cat.tellYourAddress();
-        cat2.tellYourAddress();
-     }
+    /*
+          cat2.tellYourAddress();
+        cat2.tellyourName();
+      */
+    }
     //test
 
    
--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -1,12 +1,18 @@
 package com.cc.java;
 
 public class Cat {
-
+    String furColor;
+    String name;
+    int age;
     Cat (){
-        System.out.println("xiaomi");
+        System.out.println("we build a cat");
+        furColor = "black";
     }
     public void SayHello () {
-        System.out.println("Miau");
+        System.out.println("Miau "+"my color is " +this.furColor);
+    }
+    public void tellyourName (){
+        System.out.println(this.name);
     }
     public void tellYourAddress(){
         System.out.println(this);

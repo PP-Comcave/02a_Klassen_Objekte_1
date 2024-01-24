d949140d86251507bfebb8d5ed7213c7c811f5b0
J1991Ha <jasper.haag@icloud.com>
2024-01-24 10:12:09 +0100

    Add cat

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,8 +4,10 @@ public class App {
     
     public static void main(String[] args) {
 
-
-    }
+        Cat cat = new Cat();
+        System.out.println(cat);
+        cat.SayHello();
+     }
     //test
 
    
---
+++ com/cc/java/Cat.java
@@ -0,0 +1,11 @@
+package com.cc.java;
+
+public class Cat {
+
+    Cat (){
+        System.out.println("xiaomi");
+    }
+    public void SayHello (){
+        System.out.println("Miau");
+    }
+}
